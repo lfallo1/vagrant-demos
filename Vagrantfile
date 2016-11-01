ed95646fc41717da979f50f19ad16d556dfb9978
Lance Fallon <Lance Fallon>
2016-10-31 22:09:31 -0400

    provisioning

--- Vagrantfile
+++ Vagrantfile
@@ -6,7 +6,7 @@ Vagrant.configure("2") do |config|
   config.vm.box = "ubuntu/trusty64"
   config.vm.hostname = "tomcat8"
 
-  # config.vm.network "forwarded_port", guest: 80, host: 8080
+  config.vm.network "forwarded_port", guest: 8080, host: 8080
 
   config.vm.provision "shell", path: "provision-tomcat.sh"
 
--- provision-tomcat.sh
+++ provision-tomcat.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+### BEGIN INIT INFO
+# Provides:          scriptname
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Start daemon at boot time
+# Description:       Enable service provided by daemon.
+### END INIT INFO
+
 apt-get update -y
 apt-get upgrade -y
 apt-get install -y nano git openjdk-7-jdk openjdk-7-jre-headless
+
+cd /usr/local
+wget http://apache.cs.utah.edu/tomcat/tomcat-8/v8.0.38/bin/apache-tomcat-8.0.38.tar.gz
+tar -xzvf apache-tomcat-8.0.38.tar.gz
+ln -s apache-tomcat-8.0.38.tar.gz tomcat8
+
+sudo cp /vagrant/tomcat8 /etc/init.d/tomcat8
+sudo chmod 755 /etc/init.d/tomcat8
+update-rc.d tomcat8 defaults
+service tomcat8 start
